401a4239046ac4ce645d79e3c235fedac809523b
SadovskiiAleks <alekssadovskij@yandex.ru>
2023-03-05 19:10:59 +0300

    add new version

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,7 @@
 package org.example;
 
 import java.util.*;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-import java.util.concurrent.FutureTask;
+import java.util.concurrent.*;
 
 public class Main {
 
@@ -13,50 +10,21 @@ public class Main {
         for (int i = 0; i < texts.length; i++) {
             texts[i] = generateText("aab", 30_000);
         }
-        long startTs = System.currentTimeMillis(); // start time
 
         List<Future> futures = new ArrayList<>();
+        final ExecutorService threadPool = Executors.newFixedThreadPool(texts.length);
 
+        long startTs = System.currentTimeMillis(); // start time
         for (String text : texts) {
-            Callable myLogic = () -> {
-                int maxSize = 0;
-                for (int i = 0; i < text.length(); i++) {
-                    for (int j = 0; j < text.length(); j++) {
-                        if (i >= j) {
-                            continue;
-                        }
-                        boolean bFound = false;
-                        for (int k = i; k < j; k++) {
-                            if (text.charAt(k) == 'b') {
-                                bFound = true;
-                                break;
-                            }
-                        }
-                        if (!bFound && maxSize < j - i) {
-                            maxSize = j - i;
-                        }
-                    }
-                }
-                System.out.println(text.substring(0, 100) + " -> " + maxSize);
-                return maxSize;
-            };
-            FutureTask<Integer> future = new FutureTask<>(myLogic);
-
-            futures.add(future);
-        }
-        for (Future future : futures) {
-            new Thread((Runnable) future).start();
-            //Почему тут нужно обернуть в Runnable ?
+            futures.add(threadPool.submit(new GivedCode(text)));
         }
 
         int max = 0;
-
         for (Future future : futures) {
-            if ((Integer) future.get() > max) {
-                max = (Integer) future.get();
-            }
+            max = Math.max((int) future.get(), max);
         }
         System.out.println("Max value is - " + max);
+        threadPool.shutdown();
 
         long endTs = System.currentTimeMillis(); // end time
 
